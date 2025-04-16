3d1b9bae98684e1edd53ad9bf826f9ac3537df17
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-16 19:48:01 +0200

    updated Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,7 @@ import cart from './assets/img/cart.svg'
 
 export const Header = () => {
     return (
-        <div className="Header">
+        <div className="header">
             <img src={logo} alt=""/>
             <img src={cart} alt=""/>
         </div>
