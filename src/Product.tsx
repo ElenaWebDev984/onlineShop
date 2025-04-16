edb2c42df7ec67c05e3e35e2652aa0862f47bc41
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-16 23:48:54 +0200

    added arrowBack

--- src/Product.tsx
+++ src/Product.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import type {  ProductType } from "./BestSellers.tsx";
 import rating from "./assets/img/rating.svg";
 import cartWhite from "./assets/img/cartWhite.svg";
-import {useParams} from "react-router";
+import {Link, useParams} from "react-router";
+import arrowBack from "./assets/img/arrowBack.svg";
 
 export const Product = () => {
     const [product, setProduct] = useState<ProductType | null>(null);
@@ -28,7 +29,12 @@ export const Product = () => {
 
     return (
         <div>
-            <div>Заглушка. Понадобится чуть позже. Не удаляйте :)</div>
+            <div className="arrowBack">
+                <Link to={"/"}>
+                    <img src={arrowBack} alt="arrowBack" />
+                    Back to Best Seller
+                </Link>
+            </div>
 
             <div className="product">
                 <img src={product.image} alt="" />
