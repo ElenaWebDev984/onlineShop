957c0721d38483eddf8f654f2382fa7322d03602
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-16 18:26:41 +0200

    added BestSellersComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import {Header} from "./Header.tsx";
+import {BestSellers} from "./BestSellers.tsx";
 
 function App() {
     return (
         <div className="appContainer">
-            <Header />
+            <Header/>
+            <BestSellers/>
         </div>
     )
 }
