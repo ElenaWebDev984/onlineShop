da2f00ff7ec675a11d715967a4dca77de00f8f5c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-16 22:02:00 +0200

    added path for Product

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Header} from "./Header.tsx";
 import {BestSellers} from "./BestSellers.tsx";
 import {Route, Routes} from "react-router";
+import {Product} from "./Product.tsx";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Header/>
             <Routes>
                 <Route path={'/'} element={<BestSellers/>} />
+                <Route path={'product'} element={<Product/>} />
             </Routes>
         </div>
     )
