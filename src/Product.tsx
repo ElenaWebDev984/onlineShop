17a7df3908ab47c8813d817a3d234abc6dbe55d3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-18 14:06:51 +0200

    added button.active

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,41 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import type {  ProductType } from "./BestSellers.tsx";
+import { Link, useParams } from "react-router";
+import type { ProductType } from "./BestSellers.tsx";
 import rating from "./assets/img/rating.svg";
 import cartWhite from "./assets/img/cartWhite.svg";
-import {Link, useParams} from "react-router";
+import cart from "./assets/img/cart.svg";
 import arrowBack from "./assets/img/arrowBack.svg";
-import {Reviews} from "./Reviews.tsx";
+import { Reviews } from "./Reviews.tsx";
 
 export const Product = () => {
     const [product, setProduct] = useState<ProductType | null>(null);
 
-    const {productId} = useParams();
-    console.log(productId)
+    const [isProductInCart, setIsProductIncart] = useState<boolean>(false);
+
+    const { productId } = useParams();
 
     useEffect(() => {
         axios
-            .get(`https://masterclass.kimitsu.it-incubator.io/api/products/${productId}`)
-            .then((res) => {
-                const product = res.data;
-                setProduct(product);
-            });
+            .get(
+                `https://masterclass.kimitsu.it-incubator.io/api/products/${productId}`,
+            )
+            .then((res) => setProduct(res.data));
     }, []);
 
+    const addProductToCartHandler = () => {
+        alert("Товар успешно добавлен в корзину");
+        setIsProductIncart(true);
+    };
 
     if (product === null) {
-        return <h1>Loading...</h1>
+        return <h2>Продукт еще грузится ...</h2>;
     }
 
-
     return (
-        <div>
+        <>
             <div className="arrowBack">
                 <Link to={"/"}>
                     <img src={arrowBack} alt="arrowBack" />
                     Back to Best Seller
                 </Link>
             </div>
+
             <div className="product">
                 <img src={product.image} alt="" />
                 <div className="info">
@@ -50,14 +55,19 @@ export const Product = () => {
                         <p>{product.category}</p>
                     </div>
                     <p className="description">{product.description}</p>
-                    <button>
-                        <img src={cartWhite} alt="" />
-                        Add to cart
+
+                    <button
+                        onClick={addProductToCartHandler}
+                        className={`button ${isProductInCart ? "active" : ""}`}
+                    >
+                        <img src={isProductInCart ? cart : cartWhite} alt="cart icon" />
+                        {isProductInCart ? "Go to cart" : "Add to cart"}
                     </button>
                 </div>
             </div>
-            <Reviews/>
-        </div>
+
+            <Reviews />
+        </>
     );
 };
 
